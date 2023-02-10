8194e13457f4dfdebe7f011ad9307de542ab6f06
Sam Knight <samknight673@googlemail.com>
2023-02-10 16:42:58 +0000

    web app start

---
+++ web_app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template, request, session, Response
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('java.html')
+
+@app.route('/process_times/<string:time>', methods=['POST'])
+def processtimes(time):
+    times = json.loads(time)
+    print()
+    print(time)
+    return('/')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
