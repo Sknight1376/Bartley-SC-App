7e11c32afa5b65c80b9e7213a6af43153366b94f
Sam Knight <samknight673@googlemail.com>
2023-02-15 16:51:15 +0000

    Development work day 3

--- static/js/renders.js
+++ static/js/renders.js
@@ -1,17 +1,33 @@
 
 
 function appendentriesFunction(Boat, Sail) {
-    let table = document.getElementById("entries");
+    let table = document.querySelector("table#entries>tbody");
 
     let row = table.insertRow(-1);
 
     let cellone = row.insertCell(0);
     let celltwo = row.insertCell(1);
     let cellthree = row.insertCell(2);
+    let cellfour = row.insertCell(3);
 
+    row.id = `${Boat} ${Sail} row`
     cellone.innerText = Boat
     celltwo.innerText = Sail
-    cellthree.innerHTML = `<button type="button" id = "${Sail}lap">Lap</button>`
+    cellthree.innerHTML = `<button type="button" id = "${Sail} lap" value = "${Boat} ${Sail}">Lap</button>`
+    cellfour.innerHTML = `<button type="button" id = "${Sail} finish" value = "${Boat} ${Sail}">Finish</button>`
+
+    laprecorder(Boat, Sail)
+
+    function laprecorder (Boat, Sail) {
+        const lapbutton = document.getElementById(`${Sail} lap`)
+        lapbutton.addEventListener("click",() => {
+            let row = document.getElementById(`${Boat} ${Sail} row`);
+            let time = document.getElementById("Elapsed Time").innerHTML;
+            let cell = row.insertCell(4);
+            cell.innerHTML = time
+        })
+
+    }
 
 
 }
